b7aff0bb5efc20c9cd96157e1868ff45d2d9eb35
Paul <pebre79@hotmail.com>
2013-10-29 14:40:04 -0500

    Update admin.py

--- chipy/admin.py
+++ chipy/admin.py
@@ -3,7 +3,29 @@ from chipy.models import Meeting
 from chipy.models import Venue
 from chipy.models import Person
 
+from django.contrib import admin
+from chipy.models import Meeting
+from chipy.models import Venue
+from chipy.models import Person
 
+class MeetingAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Meeting
+	
+class VenueAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Venue
+	
+class PersonAdmin(admin.ModelAdmin):
+	class Meta:
+		model = Person
+	
+
+admin.site.register(Meeting)
+
+admin.site.register(Venue)
+
+admin.site.register(Person)
 admin.site.register(Meeting)
 
 admin.site.register(Venue)
