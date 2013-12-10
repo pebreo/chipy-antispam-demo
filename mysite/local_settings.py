6c0c2efadd1c3158f7bf20eb1ac8526885a2490f
Paul <pebre79@hotmail.com>
2013-12-10 00:16:10 -0600

    Update local_settings.py

--- mysite/local_settings.py
+++ mysite/local_settings.py
@@ -15,12 +15,12 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'postgres',                      # Or path to database file if using sqlite3.
+        'NAME': 'mysitedb',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'postgres',
-        'PASSWORD': '1234',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '54321',                      # Set to empty string for default.
+        'PORT': '',                      # Set to empty string for default.
     }
 }
 
